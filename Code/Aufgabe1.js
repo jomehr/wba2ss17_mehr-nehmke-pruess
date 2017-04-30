2578d359b52e4d08fd58cfb68b931b07842725b7
Unknown <tim.nehmke@gmail.com>
2017-04-30 17:42:01 +0200

    aktualisiert Aufgabe 1

--- Code/Aufgabe1.js
+++ Code/Aufgabe1.js
@@ -1,8 +1,19 @@
-var fs = require('fs');
-var _dirname = "./../Code";
+var fs = require('fs'); //File System einbinden(ermöglicht Asynchronität)
 
-fs.readFile(_dirname+"/staedte.json", "utf8", function(err, data){
-  if (err) throw err;
-  JSON.stringify(data);
-  console.log(data);
+fs.readFile(__dirname+"/staedte.json", function(err, data){ //Daten einlesen
+
+    if (err) throw err;
+
+	var tmp = data.toString(); //Binärdaten in String umwandeln
+	//console.log(tmp); //test
+	var obj = JSON.parse(tmp); //JSON-String in Javascript-Objekt umwandeln
+
+	console.log('\n');
+	//Objekt auslesen
+	for ( var i=0; i < obj.cities.length; i++){
+		console.log('Name: ' + obj.cities[i].name);
+		console.log('Land: ' + obj.cities[i].country);
+		console.log('Population: ' + obj.cities[i].population);
+		console.log('---------------------------');
+	}
 });
