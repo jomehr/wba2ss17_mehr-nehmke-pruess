0a71482cb8e9192404ddf6095699d871efdd6c52
Jan Mehr <janmehr93@gmx.de>
2017-04-30 16:16:35 +0200

    Aufgabe 1 und 2 aktualisiert

--- Code/Aufgabe1.js
+++ Code/Aufgabe1.js
@@ -3,6 +3,6 @@ var _dirname = "./../Code";
 
 fs.readFile(_dirname+"/staedte.json", "utf8", function(err, data){
   if (err) throw err;
-  data
+  JSON.stringify(data);
   console.log(data);
 });
--- Code/Aufgabe2.js
+++ Code/Aufgabe2.js
@@ -4,6 +4,5 @@ var _dirname = "./../Code";
 
 fs.readFile(_dirname+"/staedte.json", "utf8", function(err, data){
   if (err) throw err;
-  data
-  console.log(data);
+  console.log(chalk.red,data);
 });
