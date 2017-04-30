50b3a285bd5e05a04cdeee029a67497b124964e8
Unknown <spn-cos-crew@gmx.de>
2017-04-30 18:15:46 +0200

    Aufgabe3 aktuell

--- Code/Aufgabe3.js
+++ Code/Aufgabe3.js
@@ -1,8 +1,43 @@
 var fs = require('fs');
-var chalk = require('chalk');
-var _dirname = "./../Code";
+var chalk = require('chalk');   //chalk einbinden
+var obj;
 
-fs.readFile(_dirname+"/staedte.json", "utf8", function(err, data){
-  if (err) throw err;
-  console.log(chalk.red(data));
+fs.readFile(__dirname+"/staedte.json", function(err, data){ //Daten einlesen
+
+    if (err) throw err;
+
+	var tmp = data.toString();     //Daten in String umwandeln
+	obj = JSON.parse(tmp);     //in Objekt umwandeln
+    
+
+    
+    var sor = obj.cities.sort(function (a, b) {       //aufsteigend sortieren
+ 		if (a.anzahl < b.anzahl) {
+  			return 1;
+  		}
+  		if (a.anzahl > b.anzahl) {
+    		return -1;
+  		}
+	});
+	var str = JSON.stringify(sor);     //sortierte Objekte in String umwandeln
+
+	fs.writeFile(__dirname+"staedte_sortiert.json", str, function(error){      //JSON-Datai mit Sortiertem String schreiben
+
+	   if (err) throw err;
+
+	});
+	print();
 });
+
+
+function print(){
+    
+	console.log('\n');
+	//Objekt auslesen
+	for ( var i=0; i < obj.cities.length; i++){
+		console.log('Name: ' + chalk.red(obj.cities[i].name));
+		console.log('Land: ' + chalk.green(obj.cities[i].country));
+		console.log('Population: ' + chalk.blue(obj.cities[i].population));
+		console.log('---------------------------');
+	}
+};
