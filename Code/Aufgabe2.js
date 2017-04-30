01251c93b90c5ba043ff843eae1d0215aafcd844
Jan Mehr <janmehr93@gmx.de>
2017-04-30 16:33:51 +0200

    Aufgabe 2 ausgabe komplett in rot

--- Code/Aufgabe2.js
+++ Code/Aufgabe2.js
@@ -4,5 +4,5 @@ var _dirname = "./../Code";
 
 fs.readFile(_dirname+"/staedte.json", "utf8", function(err, data){
   if (err) throw err;
-  console.log(chalk.red,data);
+  console.log(chalk.red(data));
 });
