44b85cf46fb208cc97abe221705f700a1721168e
Jan Mehr <janmehr93@gmx.de>
2017-04-30 15:18:24 +0200

    Aufgabe 1 fertig

--- Code/Aufgabe1.js
+++ Code/Aufgabe1.js
@@ -1,7 +1,8 @@
 var fs = require('fs');
-var _dirname = "./../../../../Code";
+var _dirname = "./../Code";
 
 fs.readFile(_dirname+"/staedte.json", function(err, data){
   if (err) throw err;
+  data.toString(data);
   console.log(data);
-})
+});
