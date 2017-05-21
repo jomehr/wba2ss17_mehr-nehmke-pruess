9ac18400505e482cc451ffe49ae0b33f3e175443
Jan Mehr <janmehr93@gmx.de>
2017-05-21 14:39:43 +0200

    overpass api und output als json gefixed

--- Projekt/overpass.js
+++ Projekt/overpass.js
@@ -1,21 +1,21 @@
 
 var fs = require('fs')
 queryOverpass = require('query-overpass');
-var geojson;
-var data;
+var geojson
 
-var req = queryOverpass('[out:json];node(57.7,11.9,57.8,12.0)[amenity=bar];out;', function(err, geojson) {
+
+var overpass = queryOverpass('[out:json];node(57.7,11.9,57.8,12.0)[amenity=bar];out;', function(err, geojson) {
     if (!err) {
       //  data = JSON.stringify(geojson);
-        console.log(geojson);
-        data = geojson;
+      var tmp = geojson.toString();
+      console.log(geojson);
       //  data = JSON.parse(geojson);
     } else {
         console.error(err);
       }
+      var data = JSON.stringify(geojson);
+
+      fs.writeFile(__dirname+"/geojson.json", data, function(err){      //JSON-Datai mit Sortiertem String schreiben
+         if (err) throw err;
+       });
 });
-
-fs.writeFile(__dirname+"/geojson.json", data, function(err){      //JSON-Datai mit Sortiertem String schreiben
-
-   if (err) throw err;
-   });
