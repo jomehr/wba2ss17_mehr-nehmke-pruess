e949f61156afcd46138fa4fc9e6aba0351d98228
Jan Mehr <janmehr93@gmx.de>
2017-08-12 06:00:12 +0200

    gamelogik vergleicht datum mit gamedatum

--- Projekt/Dienstnutzer/gamelogic.js
+++ Projekt/Dienstnutzer/gamelogic.js
@@ -3,11 +3,22 @@ var dURL = 'https://wba2ss17-team38.herokuapp.com';
 
 module.exports = {
   getgamedata: function() {
-     var url = dURL+ '/games';
-
+    var url = dURL+ '/games';
+    var logicdata;
+    var date = Date.now();
     request.get(url, function(err, response, body) {
-      var data = JSON.parse(body);
-      console.log(data);
+      logicdata = JSON.parse(body);
+      console.log(logicdata);
+      for (var i = 0; i < logicdata.games.length; i++) {
+        console.log(logicdata.games[i][2]);
+        if (logicdata.games[i][2] === date) {
+          console.log("Datum ist gleich und Spiel wird gestartet");
+          return;
+        } else {
+          console.log("Datum stimmt nicht überein");
+        }
+      }
     });
+
   }
 };
