a25f065e85a45e8f312875ac882301f38d8a2939
Jan Mehr <janmehr93@gmx.de>
2017-08-17 06:03:56 +0200

    vergleich der clue-coords und user coords funktioniert. anschließende ausgabe folgender clues fehlt noch

--- Projekt/Dienstnutzer/gamelogic.js
+++ Projekt/Dienstnutzer/gamelogic.js
@@ -16,49 +16,63 @@ module.exports = {
       }
       var logicdata = JSON.parse(body);
       for (var i = 0; i < logicdata.games.length; i++) {    //muss an client gescgickt werden
-        if (logicdata.games[i][2] == datems) {
+        if (logicdata.games[i][2] === datems) {
           console.log("Game mit ID " + logicdata.games[i][1] +" startet jetzt!");
+          console.log(new Date(logicdata.games[i][2]).toUTCString());
           startid = logicdata.games[i][1];
           gamelogic.startgame(startid);
         } else {    //zum testen hier eig nur meldung "startet nicht"
           console.log("Game mit ID " + logicdata.games[i][1] +" startet nicht!");
-          startid = logicdata.games[i][1];
-          gamelogic.startgame(startid);
+          console.log(new Date(logicdata.games[i][2]).toUTCString());
+          // startid = logicdata.games[i][1];
+          // gamelogic.startgame(startid);
         }
       }
       console.log("----------");
+      console.log(new Date(datems).toUTCString());
+      console.log("-----------");
     });
   },
   startgame: function(gameid) {
     var cluenumber = 0;
     var clueurl = dURL + '/games/' + gameid + "/clues/";
     var gameurl = dURL + '/games/' + gameid;
-    console.log("Spiel " + gameid + "startet");
-    function clue() {
-      request.get(clueurl, function(err, res, body) {
-        if(err) {
-          console.log("ERROR: " + body);
-        }
-        var cluedata = JSON.parse(body);
-        console.log(cluedata[cluenumber]);  //muss an Client geschickt werden
-      }
-    }
+    console.log("Spiel " + gameid + " startet");
     request.get(gameurl, function(err, res, body) {
       if(err) {
         console.log("ERROR: " + body);
       }
-      var gamedata = JSON.parse(body);
-      console.log(gamedata.participants);
-      for (var i = 0; i < gamedata.participants.length; i++) {
-        var participantid = gamedata.participants[i].id;
-        gamelogic.getuserdata(participantid);
-        var distance = geolib.getDistance(coords,cluedata[cluenumber].coordinates);
-      }
-      if (distance <= 10) {
-        cluenumber ++;
-        console.log(cluedata[cluenumber]);
-      }
+      var cluedata = JSON.parse(body);
+      console.log("CLUEDATA: "+cluedata.clues);
+      console.log("ERSTERCLUE: "+cluedata.clues[0]);
+
+      request.get(gameurl, function(err, res, body) {
+        if(err) {
+          console.log("ERROR: " + body);
+        }
+        var gamedata = JSON.parse(body);
+        console.log(gamedata.participants);
+        for (var i = 0; i < gamedata.participants.length; i++) {
+          var participantid = gamedata.participants[i].userid;
+          var userurl = dURL+ '/users/' + participantid;
+          request.get(userurl, function(err, res, body) {
+            if(err) {
+              console.log("ERROR: " + body);
+            }
+            var userdata = JSON.parse(body);
+            console.log("USERDATA: "+userdata);
+            var coords = userdata.coordinates;
+            console.log(coords);
+            var distance = geolib.getDistance(coords, gamedata.clues[cluenumber].coordinates);
+            if (distance <= 10) {
+              cluenumber ++;
+              console.log("DISTANCE" +distance);
+            }
+          });
+        }
+      });
     });
+
   },
   getuserdata: function(userid) {
     url = dURL+ '/users/' + userid;
@@ -66,10 +80,11 @@ module.exports = {
       if(err) {
         console.log("ERROR: " + body);
       }
+      console.log("1" +body);
       var userdata = JSON.parse(body);
       var coords = userdata.coordinates;
-      console.log(coords);
+      console.log("2" +coords);
       return coords;
-    }
+    });
   }
 }
